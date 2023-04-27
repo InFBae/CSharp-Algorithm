9d166a3001600e3c70010029fde455ad1aa6fe9c
Jung BoBae <acarid27@gmail.com>
2023-04-27 19:44:27 +0900

    feat: Add DesignTechnique Project

---
+++ 09.DesignTechnique/BackTracking.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.DesignTechnique
+{
+    internal class BackTracking
+    {
+        /******************************************************
+         * 백트래킹 (Backtracking)
+         * 
+         * 모든 경우의 수를 전부 고려하는 알고리즘
+         * 후보해를 검증 도중 해가 아닌경우 되돌아가서 다시 해를 찾아가는 기법
+         ******************************************************/
+
+        // 예시 - N개의 퀸(체스판의 서로 공격할 수 없는 퀸 N개 놓기)
+
+
+    }
+}
---
+++ 09.DesignTechnique/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace _09.DesignTechnique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi hanoi = new Hanoi(5);
+
+            hanoi.Move(hanoi.Size, 0, 2, 1);
+            Console.WriteLine("Completed");
+        }
+
+        public class Hanoi
+        {
+            const int DefaultSize = 5;
+            private int size;
+            public int Size { get { return size; } }
+            Stack<int>[] hanoi = new Stack<int>[3];
+            public Hanoi(int size = DefaultSize)
+            {
+                this.size = size;
+                for (int i = 0; i < 3; i++)
+                {
+                    hanoi[i] = new Stack<int>();
+                }
+
+                for (int i = size; i > 0; i--)
+                {
+                    hanoi[0].Push(i);
+                }
+            }
+
+            public void Move(int count, int source, int dest, int other)
+            {
+                if (count == 1)
+                {
+                    MoveBoard(source, dest);
+                    return;
+                }
+                Move(count - 1, source, other, dest);
+                Move(count - 1, other, dest, source);
+            }
+            private void MoveBoard(int source, int dest)
+            {
+                hanoi[dest].Push(hanoi[source].Pop());
+            }
+        }
+       
+    }
+}
